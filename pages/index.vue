ce3cb377a40c49ff90fb21ab8258daa736c79655
Funasaka Takumi <takumi.funasaka@gmail.com>
2021-05-24 03:58:00 +0900

    add frame and datetime

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="photoletter">
+  <div id="rice-time">
     <div class="container" v-if="!isGenerated">
       <section class="hero has-text-centered is-fullheight">
         <div class="hero-body">
@@ -7,7 +7,7 @@
             <figure class="image p-5">
               <img src="../assets/images/ricetime.png" />
             </figure>
-            <div class="button is-black" @click="generate">
+            <div class="button is-black has-text-weight-bold" @click="generate">
               check rice.
             </div>
           </div>
@@ -22,13 +22,21 @@
         <div class="container">
           <div class="columns is-centered">
             <div class="column has-text-centered is-half">
-              <img
-                class="box"
-                id="canvasImage"
-                src="../assets/images/rice.jpeg"
-              />
-              <p v-show="uploaded" class="pt-3 pb-6 mb-6">
-                画像を長押しして保存できます
+              <div class="image-wrapper mb-0">
+                <img
+                  class=""
+                  id="canvasImage"
+                  src="../assets/images/rice.jpeg"
+                />
+                <img
+                  class=""
+                  id="canvasFrame"
+                  src="../assets/images/frame.png"
+                />
+              </div>
+
+              <p class="pt-3 pb-6 mb-6 has-text-weight-bold	">
+                {{ displayedTime }}
               </p>
             </div>
           </div>
@@ -46,6 +54,7 @@ import { storage } from "@/plugins/firebase";
 export default {
   data() {
     return {
+      displayedTime: "",
       isGenerationStarted: false,
       isGenerated: false
     };
@@ -58,6 +67,14 @@ export default {
     generate() {
       this.isGenerationStarted = true;
       this.isGenerated = true;
+      var now = new Date();
+      var year = now.getFullYear();
+      var month = now.getMonth() + 1;
+      var day = now.getDate();
+      var hour = now.getHours();
+      var minute = now.getMinutes();
+      this.displayedTime =
+        year + "/" + month + "/" + day + " " + hour + ":" + minute;
       setTimeout(() => {
         this.isGenerationStarted = false;
       }, 4000);
@@ -73,39 +90,16 @@ export default {
 @import "../assets/styles/variables.scss";
 
 * {
-  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
+  font-family: "Avenir Next", Arial, "Hiragino Kaku Gothic ProN",
     "Hiragino Sans", Meiryo, sans-serif;
 }
-#photoletter {
+#rice-time {
   color: $main3;
   position: relative;
 
   .hero {
     .hero-body {
       position: relative;
-
-      .icon {
-        position: absolute;
-        bottom: 0;
-      }
-    }
-
-    .hero-foot {
-      .scroll {
-        color: #535353;
-        line-height: 3.5;
-      }
-      .image {
-        animation: vertical 1s ease-in-out infinite alternate;
-      }
-      @keyframes vertical {
-        0% {
-          transform: translateY(0px);
-        }
-        100% {
-          transform: translateY(10px);
-        }
-      }
     }
   }
 
@@ -171,6 +165,28 @@ export default {
     background-color: white;
     height: 100vh;
     z-index: -10;
+
+    .image-wrapper {
+      position: relative;
+      height: 0;
+      padding-bottom: 100%;
+
+      #canvasImage {
+        position: absolute;
+        padding: 10%;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+      #canvasFrame {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    }
   }
 }
 </style>
